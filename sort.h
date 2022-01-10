0463abd49c7f43a0344a588e960f7afcf99a3e9a
kongoloa <aristoteshako@gmail.com>
2022-01-10 12:34:06 -0800

    quick sort

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-size_t partition(int array[], size_t size);
+int partition(int *a, int l, int h);
 void print_sort(int array[], size_t size, int init);
 
 /* Prototypes for advanced tasks */
